462eb754652bd9d6bee126ede4be97772433fb9d
jtufekovic <jelena.tufekovic@gmail.com>
2025-04-14 18:38:08 +0200

    simple api crud example (in-memory list)

---
+++ EmployeePortal/EmployeePortal.WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmployeePortal.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private static List<Employee> _employees = new List<Employee>();
+
+        //data anotation example for route
+        [HttpGet("getAll")]
+        public IActionResult GetAll()
+        {
+            if (_employees == null)
+            {
+                return NotFound();
+            }
+            return Ok(_employees);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            Employee? employee = _employees.FirstOrDefault(e => e.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Employee newEmployee)
+        {
+            newEmployee.Id = Guid.NewGuid();
+            _employees.Add(newEmployee);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, Employee updatedEmployee)
+        {
+            Employee? currentEmployee = _employees.FirstOrDefault(e => e.Id == id);
+
+            if (currentEmployee == null)
+            {
+                return BadRequest();
+            }
+
+            currentEmployee.FirstName = updatedEmployee.FirstName;
+            currentEmployee.LastName = updatedEmployee.LastName;
+            currentEmployee.WorkDepartmentId = updatedEmployee.WorkDepartmentId;
+            currentEmployee.DateOfBirth = updatedEmployee.DateOfBirth;
+            return Ok();
+        }
+
+        //example of sending complex object like query params and using rest models
+        [HttpPut("{id}/updateFromQuery")]
+        public IActionResult UpdateFromQuery(Guid id, [FromQuery] UpdateEmployeeRest updatedEmployee)
+        {
+            Employee? currentEmployee = _employees.FirstOrDefault(e => e.Id == id);
+
+            if (currentEmployee == null)
+            {
+                return BadRequest();
+            }
+
+            currentEmployee.FirstName = updatedEmployee.FirstName;
+            currentEmployee.LastName = updatedEmployee.LastName;
+            currentEmployee.WorkDepartmentId = updatedEmployee.WorkDepartmentId;
+            currentEmployee.DateOfBirth = updatedEmployee.DateOfBirth;
+            return Ok(currentEmployee);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var employee = _employees.FirstOrDefault(e => e.Id == id);
+
+            if (employee is null)
+            {
+                return BadRequest();
+            }
+
+            _employees.Remove(employee);
+            return Ok();
+        }
+    }
+}
---
+++ EmployeePortal/EmployeePortal.WebApi/UpdateEmplyeeRest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeePortal.WebApi
+{
+    public class UpdateEmployeeRest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public Guid? WorkDepartmentId { get; set; }
+    }
+}
